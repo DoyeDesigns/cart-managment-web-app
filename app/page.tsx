4075b36150a183a81dc07e4ddceda219606ea2d8
Ogoba David Edouye <114475043+DoyeDesigns@users.noreply.github.com>
2023-12-14 13:50:35 +0000

    moved navbar to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
 import ProductsList from '@/components/productCard'
-import NavBar from '@/components/navbar'
 
 export default function Home() {
   return (
     <main>
-      <NavBar />
       <ProductsList />
     </main>
   )
